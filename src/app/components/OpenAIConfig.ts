cfe9c8eafaa92e6fef0519afd7173baeac93bcf4
sparsh47 <sparshshandilya123@gmail.com>
2025-03-20 10:23:50 +0530

    api key fix

--- src/app/components/OpenAIConfig.ts
+++ src/app/components/OpenAIConfig.ts
@@ -14,7 +14,7 @@ const genAI = new GoogleGenerativeAI(apiKey);
 
 
 export async function conversation(message: string) {
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
     const result = await model.generateContent(message);
 

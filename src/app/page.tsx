cef44d0b46407a38f9522a25518adf416c5844ae
sparsh47 <sparshshandilya123@gmail.com>
2025-06-02 19:56:00 +0530

    made home page responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,57 +9,89 @@ import Navbar from "../components/Navbar";
 import Image from "next/image";
 import hero from "../../public/hero.png";
 import line from "../../public/line.png";
+import {PlusIcon, PencilSquareIcon} from "@heroicons/react/16/solid";
 
 export default function Home() {
   const [url, setUrl] = useState("");
   const router = useRouter();
 
   return (
-    <>
-      <div
-        className="absolute w-[800px] h-[800px] bg-[#7752FE] rounded-[50%] top-[-300px] left-[-200px] z-[-20]"
-        style={{ filter: "blur(150px)" }}
-      ></div>
-      <div
-        className="absolute w-[800px] h-[800px] bg-[#7752FE] rounded-[50%] top-[600px] right-[100px] z-[-20]"
-        style={{ filter: "blur(150px)" }}
-      ></div>
-      <div
-        className="absolute w-[300px] h-[300px] bg-[#7752FE] rounded-[50%] bottom-[-500px] left-[10px] z-[-20]"
-        style={{ filter: "blur(130px)" }}
-      ></div>
+        <div className="relative overflow-hidden min-h-screen">
+            <div
+                className="absolute w-[800px] h-[800px] bg-[#7752FE] rounded-full top-[-300px] left-[-200px] z-[-20]"
+                style={{ filter: "blur(150px)" }}
+            ></div>
+
+            <div
+                className="absolute w-[800px] h-[800px] bg-[#7752FE] rounded-full top-[600px] right-[100px] z-[-20]"
+                style={{ filter: "blur(150px)" }}
+            ></div>
+
+            <div
+                className="absolute w-[300px] h-[300px] bg-[#7752FE] rounded-full bottom-[-500px] left-[10px] z-[-20]"
+                style={{ filter: "blur(130px)" }}
+            ></div>
       <Navbar />
       <main
-        className="flex flex-col items-center gap-12 p-24"
+        className="flex flex-col items-center gap-12 pt-48 sm:py-24 px-5 sm:px-12 md:p-24"
         style={{ height: "calc(100vh - 80px)" }}
       >
         <div className="flex flex-col items-center">
-          <div className="text-8xl font-black text-center">
+          <div className="text-4xl sm:text-6xl lg:text-7xl xl:text-8xl font-black text-center">
             Make Documents.
             <br />
             <span className="flex justify-center">
               Create&nbsp;
-              <span className="flex flex-col items-center">
+              <span className="relative flex flex-col items-center mb-12">
                 Impact.
                 <Image
-                  src={line}
-                  alt="underline"
-                  width={370}
-                  className="-mt-12 z-[-1]"
+                    src={line}
+                    alt="underline"
+                    width={370}
+                    className="-bottom-5 sm:-bottom-10 lg:-bottom-12 xl:-bottom-16 z-[-1] absolute"
                 />
               </span>
             </span>
           </div>
           <div
-            className="text-center -mt-5 font-medium"
-            style={{ fontSize: "22px", lineHeight: "28px" }}
+            className="text-center -mt-5 font-medium text-base sm:text-[22px] leading-5 sm:leading-7"
           >
             Collaborate Effortlessly, Create Together: Your Words, Supercharged
-            by AI. <br />
+            by AI. <br className="sm:block hidden" />
             Empowering Teamwork with our Text Editor.
           </div>
         </div>
-        <div className="flex items-center gap-5">
+          <div
+              className="flex w-full items-center space-x-2 rounded-[100px]  border border-gray-600 pr-[5px]"
+              style={{
+                  boxShadow: "2px 4px 12px rgba(0, 0, 0, 0.2)",
+              }}
+          >
+              <input
+                  type="url"
+                  placeholder="Your link"
+                  className="h-[50px] w-full outline-none border-none pl-5 bg-transparent placeholder:text-gray-700"
+                  value={url}
+                  onChange={(e) => setUrl(e.target.value)}
+              />
+              <div className="flex items-center justify-center gap-1">
+                  <Button
+                      className="h-[40px] rounded-l-full rounded-r-md font-medium text-md"
+                      type="submit"
+                      onClick={() => router.push(url)}
+                  >
+                      Join
+                  </Button>
+                  <Button
+                      className="h-[40px] rounded-r-full rounded-l-md font-medium text-md"
+                      type="submit"
+                      onClick={() => router.push(url)}
+                  >
+                      <PlusIcon className="w-5 h-5 text-white" />
+                  </Button>
+              </div>
+          </div>
+        <div className="hidden md:flex items-center gap-5">
           <Button
             className="h-[45px] rounded-[100px] py-5 px-8 font-medium text-md"
             onClick={() => router.push(`/documents/${uuidV4()}`)}
@@ -91,15 +123,13 @@ export default function Home() {
             </Button>
           </div>
         </div>
-        <Image
-          src={hero}
-          alt="hero-image"
-          className="w-[100rem] rounded-[20px] shadow-xl border border-gray-200"
-          style={{
-            boxShadow: "0 6px 20px rgba(0, 0, 0, 0.3)",
-          }}
-        />
+          <Image
+              src={hero}
+              alt="hero-image"
+              className="w-full max-w-screen-xl h-auto rounded-[20px] shadow-xl border border-gray-200 object-cover"
+              style={{ boxShadow: "0 6px 20px rgba(0, 0, 0, 0.3)" }}
+          />
       </main>
-    </>
+    </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,29 +2,32 @@
 
 import Image from "next/image";
 import logo from "../../public/logo1.png";
-import { useRouter } from "next/navigation";
 import {SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from "@clerk/nextjs";
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
+import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
+import {useState} from "react";
 
 const Navbar = () => {
-  const router = useRouter();
+
+    const [openMenu, setOpenMenu] = useState<boolean>(false);
 
   return (
-    <nav className="w-full h-[80px] flex items-center justify-between bg-transparent">
-        <div className="relative left-10"></div>
-      <Image
-        src={logo}
-        alt="logo"
-        width={280}
-        height={280}
-        style={{ cursor: "pointer" }}
-        onClick={() => router.push("/")}
-        className="absolute left-1/2 -translate-x-1/2 z-[50] mix-blend-multiply"
-      />
-        <div className="relative right-10">
+    <nav className="absolute top-0 w-full h-[80px] flex items-center justify-between bg-transparent">
+        <div className="relative left-10 hidden md:block"></div>
+      <Link href="/" className="relative md:translate-x-1/3">
+          <Image
+              src={logo}
+              alt="logo"
+              width={280}
+              height={180}
+              style={{ cursor: "pointer" }}
+              className="z-[50] h-16 w-auto mix-blend-multiply"
+          />
+      </Link>
+        <div className="relative right-4 sm:right-10">
             <SignedOut>
-                <div className="flex items-center justify-center gap-3">
+                <div className="hidden md:flex items-center justify-center gap-3">
                     <SignInButton>
                         <Button
                             className="h-[40px] rounded-full py-5 px-8 font-medium text-sm"
@@ -41,13 +44,33 @@ const Navbar = () => {
                         </Button>
                     </SignUpButton>
                 </div>
+                <div className="relative flex-col flex md:hidden">
+                    <EllipsisVerticalIcon className="w-8 h-8 text-gray-950 hover:bg-[rgba(0,0,0,0.1)] rounded-full cursor-pointer p-1" onClick={()=>setOpenMenu(prev=>!prev)} />
+                    {openMenu && <div
+                        className="absolute flex flex-col w-32 p-2 bg-[rgba(255,255,255,0.5)] rounded-sm shadow-md -bottom-24 right-2">
+                        <SignInButton>
+                            <Button
+                                className="hover:bg-[rgba(0,0,0,0.1)] bg-transparent shadow-none text-black"
+                            >
+                                Sign In
+                            </Button>
+                        </SignInButton>
+                        <SignUpButton>
+                            <Button
+                                className="hover:bg-[rgba(0,0,0,0.1)] bg-transparent shadow-none text-black"
+                            >
+                                Sign Up
+                            </Button>
+                        </SignUpButton>
+                    </div>}
+                </div>
             </SignedOut>
             <SignedIn>
-                <div className="flex items-center justify-center gap-5">
-                    <Link href="/dashboard">Dashboard</Link>
+                <div className="flex items-center justify-center gap-3">
+                    <Link href="/dashboard" className="text-sm sm:text-base font-medium">Dashboard</Link>
                     <UserButton appearance={{
                         elements: {
-                            userButtonAvatarBox: "w-10 h-10"
+                            userButtonAvatarBox: "w-9 h-9 sm:w-10 sm:h-10"
                         }
                     }} />
                 </div>
